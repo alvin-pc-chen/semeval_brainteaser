e42b0d1144bd95533056e711788855e6331ca1be
alvin-pc-chen <alvin.chen@colorado.edu>
2023-11-30 19:31:34 -0700

    Clean Data for npy, txt, and tsv
    1. Got LF to CRLF warning from GitHub
    2. Kdiff shows old train and new train are the same
    3. new eval and just eval folders are also equal
    4. accidentally edited original files, will revert

--- npyconverter.py
+++ npyconverter.py
@@ -1,5 +1,6 @@
 # File locations
 NPY_DATA_PATH = "data/data-npy"
+CLEAN_DATA_PATH = "data/data-npy-clean"
 TXT_DATA_PATH = "data/data-txt"
 TSV_DATA_PATH = "data/data-tsv"
 
@@ -24,10 +25,34 @@ def find_npy_files(path):
 
 
 def remove_lines(string):
+    """
+    Do a little clean up on the string:
+    1. remove new lines
+    2. remove double spaces
+    3. remove non-breaking spaces
+    4. remove leading spaces
+    5. remove trailing spaces
+    6. remove trailing commas
+    7. remove period + comma
+    TODO:
+    3. fix typos?
+    """
     if isinstance(string, str):
-        return string.replace('\n', ' ')
-    else:
+        string = string.replace("\n", "")
+        string = string.replace("\xa0", " ")
+        string = string.replace("  ", " ")
+        string = string.replace(".,", ".")
+        if string.startswith(" "):
+            string = string[1:]
+        if string.endswith(" "):
+            string = string[:-1]
+        if string.endswith(","):
+            string = string[:-1]
         return string
+    elif isinstance(string, list):
+        return [remove_lines(x) for x in string]
+    else:
+        return str(string)
 
 
 def npy_to_txt(npy_path, txt_path):
@@ -43,12 +68,11 @@ def npy_to_txt(npy_path, txt_path):
             for key in npy[i]:
                 f.write(remove_lines(key) + ": ")
                 if isinstance(npy[i][key], list):
-                    for val in npy[i][key]:
-                        f.write(str(remove_lines(val)) + ", ")
-                    f.write("\n")
+                    line = "".join(str(x) + ", " for x in npy[i][key])
+                    f.write(remove_lines(line) + "\n")
                 else:
-                    f.write(str(remove_lines(npy[i][key])) + "\n")
-            f.write("\n")
+                    f.write(remove_lines(npy[i][key]) + "\n")
+            f.write("\n\n")
 
 
 def npy_to_tsv(npy_path, tsv_path):
@@ -58,20 +82,36 @@ def npy_to_tsv(npy_path, tsv_path):
     :param tsv_path: path to save tsv files
     """
     npy = np.load(npy_path, allow_pickle=True)
-    with open(tsv_path, 'wt') as f:
-        tsv_writer = csv.writer(tsv_path, delimiter='\t')
+    with open(tsv_path, "wt") as outfile:
+        tsv_writer = csv.writer(outfile, delimiter="\t")
         header = []
         for key in npy[0]:
-            header.append(key)
+            header.append(remove_lines(key))
         tsv_writer.writerow(header)
         for i in range(len(npy)):
             row = []
             for key in npy[i]:
                 if isinstance(npy[i][key], list):
-                    row.append(", ".join(npy[i][key]))
+                    line = "".join(str(x) + ", " for x in npy[i][key])
+                    row.append(remove_lines(line))
                 else:
-                    row.append(npy[i][key])
+                    row.append(remove_lines(npy[i][key]))
             tsv_writer.writerow(row)
+
+
+def clean_npy(npy_path):
+    """
+    Clean up the npy file with remove_lines()
+    """
+    npy = np.load(npy_path, allow_pickle=True)
+    save_path = re.sub(r"data-npy", "data-npy-clean", npy_path)
+    for i in range(len(npy)):
+        for key in npy[i]:
+            if isinstance(npy[i][key], list):
+                npy[i][key] = [remove_lines(x) for x in npy[i][key]]
+            else:
+                npy[i][key] = remove_lines(npy[i][key])
+    np.save(npy_path, npy, allow_pickle=True)
 
 
 def main():
@@ -80,6 +120,7 @@ def main():
         tsv_path = re.sub(r"npy", "tsv", file)
         npy_to_txt(file, txt_path)
         npy_to_tsv(file, tsv_path)
+        clean_npy(file)
 
 
 if __name__ == "__main__":
